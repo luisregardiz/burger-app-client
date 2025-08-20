b3b0637cb245d14661c04bba434178ff1acac74a
luisregardiz <gadeeks@gmail.com>
2025-08-20 18:23:21 -0400

    feat: add user authentication flow with Turnstile and burger listing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,63 @@
-import { Badge } from "./components/ui/badge"
-import { Button } from "./components/ui/button"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./components/ui/card"
-import { useGetBurgers } from "./hooks/use-get-burgers"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { useState } from "react"
+import Turnstile, { useTurnstile } from "react-turnstile"
 
-function App() {
-  const { data, loading, error } = useGetBurgers()
 
-  if (loading) return <div>Loading...</div>
-  if (error) return <div>Error: {error}</div>
+import { toast, Toaster } from "sonner"
+import { useNavigate } from "react-router"
+
+export default function App() {
+  const turnstile = useTurnstile()
+  const navigate = useNavigate()
+
+  const [cedula, setCedula] = useState("")
+  const [token, setToken] = useState<string | null>(null);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if (!token) {
+      toast("Token no encontrado")
+      return
+    }
+    setCedula("")
+    navigate("/burger")
+  }
+
+
+  const onVerify = (token: string) => {
+    setToken(token);
+  };
+
+  const onExpire = () => {
+    setToken(null);
+  };
+
 
   return (
-    <>
-      <div className="container mx-auto pt-6">
-        <h2 className="text-2xl font-bold">
-          Burger app
-        </h2>
-        <div className="flex flex-wrap items-center gap-4 my-10">
-          {data?.map((burger) => (
-            <Card key={burger._id} className="w-xs pt-0">
-              <img src="https://lumenor.ai/cdn-cgi/imagedelivery/F5KOmplEz0rStV2qDKhYag/df99d90c-733b-471a-a1ff-23f09fdd8500/source" alt="Burger" className="rounded-t-xl" />
-              <CardHeader>
-                <CardTitle>{burger.name}</CardTitle>
-                <Badge variant="secondary" className="mt-2">{burger.veg ? "Vegetarian" : "Meat"}</Badge>
-              </CardHeader>
-              <CardContent>
-                <CardDescription>USD$ {burger.price}</CardDescription>
-              </CardContent>
-              <CardFooter>
-                <Button>Add to cart</Button>
-              </CardFooter>
-            </Card>
-          ))}
-        </div>
+    <div className="container mx-auto">
+      <Toaster />
+      <div className="flex justify-center items-center h-screen">
+        <Card className="w-[400px]">
+          <CardHeader>
+            <CardTitle className="text-xl">Acceder a la app</CardTitle>
+            <CardDescription>Agrega tu cedula de identidad para iniciar sesión</CardDescription>
+          </CardHeader>
+          <form onSubmit={handleSubmit} className="flex flex-col gap-4" >
+            <CardContent>
+              <Input placeholder="Cedula" value={cedula} onChange={(e) => setCedula(e.target.value)} />
+            </CardContent>
+            <div className="px-6">
+              <Turnstile sitekey="3x00000000000000000000FF" onVerify={onVerify} onExpire={onExpire} />
+            </div>
+            <CardFooter>
+              <Button disabled={!cedula.length || cedula.length < 7 || !token}>Acceder</Button>
+            </CardFooter>
+          </form>
+        </Card>
       </div>
-    </>
+    </div>
   )
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Route } from 'react-router'
+import Burger from './pages/burger.tsx'
+import { Routes } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/burger" element={<Burger />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
